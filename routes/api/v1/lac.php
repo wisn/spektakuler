e1d4bdcc3fa3022f3e9efc1a300774ad686f5b1c
adityaemaulana <adityaemaulana@gmail.com>
2019-05-03 14:29:34 +0700

    Init commit

--- routes/api/v1/lac.php
+++ routes/api/v1/lac.php
@@ -2,8 +2,14 @@
 
 function languageCenterRouter($router) {
     $router->group(['namespace' => 'LanguageCenter'], function () use ($router) {
-          $router->get('/', function () use ($router) {
-              return 'LanguageCenterRouter';
-          });
+      $router->group(['prefix' => 'nilai'], function () use ($router) {
+          $router->get('/list', 'NilaiController@list');
+
+          $router->post('/new', 'GedungController@new');
+
+          // $router->put('/{nama}/update', 'GedungController@update');
+
+          // $router->delete('/{nama}/remove', 'GedungController@remove');
+      });
     });
 }
