8a88c8f8d6d337134a792548250df0dd372bbbe5
bbadrus123 <bbadrussholehaxel@gmail.com>
2019-05-04 19:42:01 +0700

    modul alumni

--- app/Models/Alumni/Alumni.php
+++ app/Models/Alumni/Alumni.php
@@ -2,31 +2,35 @@
 
 namespace App\Models\Alumni;
 
-use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class Alumni extends Model implements AuthenticatableContract, AuthorizableContract
+class Alumni extends Model
 {
-    use Authenticatable, Authorizable;
-
     protected $table = 'alumni';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['NIM', 'tahun_kelulusan'];
+    protected $fillable = [
+        'NIM',
+        'tahun_kelulusan'
+    ];
 
     public function listAlumni()
     {
         return $this->all();
     }
 
+    public function findbyNIM($NIM) {
+        return $this->where('NIM', $NIM)->get();
+    }
 
-    public function remove($NIM) {
+    public function newAlumni($data) {
+
+        $this->NIM = $data['NIM'];
+        $this->tahun_kelulusan = $data['tahun_kelulusan'];
+
+        $this->save();
+    }
+
+    public function removeAlumni($NIM) {
         $isExists = $this->where('NIM', $NIM)->limit(1)->count() == 1;
 
         if ($isExists) {
@@ -34,9 +38,5 @@ class Alumni extends Model implements AuthenticatableContract, AuthorizableContr
         }
 
         return false;
-    }
-
-    public function findByNIM($NIM) {
-        return $this->where('NIM', $NIM)->get();
     }
 }
--- routes/api/v1/alumni.php
+++ routes/api/v1/alumni.php
@@ -4,6 +4,14 @@ function alumniRouter($router) {
     $router->group(['namespace' => 'Alumni'], function () use ($router) {
         $router->group(['prefix' => 'Alumni'], function () use ($router) {
             $router->get('/ShowAlumni', 'AlumniController@index');
+
+            $router->get('/ShowAlumni/{NIM}', 'AlumniController@findAlumni');
+
+            $router->post('/newAlumni', 'AlumniController@newAlumni');
+
+            $router->put('/{NIM}/updateAlumni', 'AlumniController@updateAlumni');
+
+            $router->delete('/{NIM}/removeAlumni', 'AlumniController@removeAlumni');
         });
     });
 }
