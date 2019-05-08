e3216ac1f317e8f6c1f9b7f3d60d6d5843bb771d
ghinadya <34832976+ghinadya@users.noreply.github.com>
2019-05-08 22:25:03 +0700

    Update hr.php

--- routes/api/v1/hr.php
+++ routes/api/v1/hr.php
@@ -2,8 +2,12 @@
 
 function humanResourceRouter($router) {
     $router->group(['namespace' => 'HumanResource'], function () use ($router) {
-        $router->get('/', function () use ($router) {
-            return 'HumanResourceRouter';
+    	 $router->group(['prefix' => 'Dosen'], function () use ($router) {
+            $router->get('/ShowDosen', 'DosenController@index');
+            $router->get('/Show/{nip_dosen}', 'DosenController@findDosen');
+            $router->post('/newDosen', 'DosenController@newDosen');
+            $router->put('/{nip_dosen}/updateDosen', 'DosenController@updateDosen');
+            $router->delete('/{nip_dosen}/removeDosen', 'DosenController@removeDosen');
         });
     });
 }
